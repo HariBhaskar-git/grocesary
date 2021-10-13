bda487d98873d08257fb08e8dbdfca7642558777
HariBhaskar-git <92365152+HariBhaskar-git@users.noreply.github.com>
2021-10-13 10:29:19 +0530

    Add files via upload

---
+++ src/app/components/masters/products/products.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ProductmasterService } from '../../../services/productmaster.service';
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  productmaster!: FormGroup;
+  constructor(private fb: FormBuilder, private service: ProductmasterService) { }
+
+  ngOnInit(): void {
+
+    this.productmaster = this.fb.group({
+      productname: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      productimage: ['', Validators.required],
+      category: ['', Validators.required]
+    })
+  }
+  saveproduct() {
+   // this.service.addregister(JSON.stringify(this.register.value)).subscribe(res => { alert(res.toString()); });
+  }
+}
