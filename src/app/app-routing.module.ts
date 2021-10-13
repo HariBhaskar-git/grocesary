5df8f261ed777ca2481f8753ea7026e25c0cc2c3
HariBhaskar-git <92365152+HariBhaskar-git@users.noreply.github.com>
2021-10-13 10:03:44 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component'
+import { LoginComponent } from './components/login/login.component'
+import { RegisterComponent } from './components/register/register.component'
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component'
+import { MastersComponent } from './components/masters/masters.component'
+import { ContactusComponent } from './components/shared/contactus/contactus.component'
+import { AboutusComponent } from './components/shared/aboutus/aboutus.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: '/shop', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'masters', component: MastersComponent },
+  { path: 'shop', component: ShoppingCartComponent },
+  { path: 'contactus', component: ContactusComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: '**', component: PageNotFoundComponent }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+
+}
